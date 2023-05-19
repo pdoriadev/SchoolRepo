20e51208a11805952d7d4abd90cee7ff0023f6e8
Peter Doria <pdoriadev@gmail.com>
2023-05-19 00:16:25 -0700

    More WIP on BST insertion algorithm.

--- CS22-StrAndAlg/SelfStudy/BinaryTree/BinaryTree/BinaryTree.cpp
+++ CS22-StrAndAlg/SelfStudy/BinaryTree/BinaryTree/BinaryTree.cpp
@@ -15,27 +15,31 @@ struct treeNode
     treeNode* Parent;
     treeNode* Left;
     treeNode* Right;
+    bool onRightSideOfTree;
     int depth;
-    treeNode(const int _data)
+    treeNode(const int _data, bool _onRightSideOfTree)
     {
         data = _data;
         Parent = NULL;
         Left = NULL;
         Right = NULL;
+        onRightSideOfTree = _onRightSideOfTree;
     }
-    treeNode(const int _data, treeNode* _parent)
+    treeNode(const int _data, treeNode* _parent, bool _onRightSideOfTree)
     {
         data = _data;
         Parent = _parent;
         Left = NULL;
         Right = NULL;
+        onRightSideOfTree = _onRightSideOfTree;
     }
-    treeNode(const int _data, treeNode * _parent, treeNode * _left, treeNode* _right)
+    treeNode(const int _data, treeNode * _parent, treeNode * _left, treeNode* _right, bool _onRightSideOfTree)
     {
         data = _data;
         Parent = _parent;
         Left = _left; 
         Right = _right;
+        onRightSideOfTree = _onRightSideOfTree;
     }
 };
 
@@ -79,7 +83,7 @@ treeNode* AddNewNodeToTree(int* arr, int arrSize, const int i, treeNode* parentN
     {
         // dynamically change BSP hierarchy as new values are encountered in order to match BSP rules
         // left child is less than parent. right child is greater than parent.
-            // all right child's descendents must be greater than the child's parent
+            // all right child's descendents m-ust be greater than the child's parent
             // all left child's descendents must be less than the child's parent
         bool greaterThan = arr[i] > parentNode->data;
         bool equal = arr[i] == parentNode->data;
@@ -89,44 +93,134 @@ treeNode* AddNewNodeToTree(int* arr, int arrSize, const int i, treeNode* parentN
         }
         if (greaterThan &&)
 
-
     }
 
     if (parentNode == NULL)
     {
-        parentNode = new treeNode(arr[i]);
+        parentNode = new treeNode(arr[i], false);
         AddNewNodeToTree(arr, arrSize, i + 1, parentNode, false);
         return parentNode;
     }
-    else if(parentNode->Left == NULL)
+    
+    if(parentNode->Left == NULL)
     {
-        parentNode->Left = new treeNode(arr[i], parentNode);
+        if (parentNode->Parent == NULL)
+        {
+            parentNode->Left = new treeNode(arr[i], parentNode, false);
+            return AddNewNodeToTree(arr, arrSize, i + 1, parentNode->Left, false);
+        }
+        parentNode->Left = new treeNode(arr[i], parentNode, parentNode->onRightSideOfTree);
         return AddNewNodeToTree(arr, arrSize, i + 1, parentNode, false);
     }
-    else if (parentNode->Right == NULL)
+
+    if (parentNode->Right == NULL)
     {
-        parentNode->Right = new treeNode(arr[i], parentNode);
+        if (parentNode->Parent == NULL)
+        {
+            parentNode->Right = new treeNode(arr[i], parentNode, true);
+            return AddNewNodeToTree(arr, arrSize, i + 1, parentNode->Left, false);
+        }
+        parentNode->Right = new treeNode(arr[i], parentNode, parentNode->onRightSideOfTree);
         return AddNewNodeToTree(arr, arrSize, i + 1, parentNode->Left, false);
     }
 }
 
-treeNode* TraverseTreeForNode(treeNode * nodeToMatch)
-{
-
-}
-
 /*************************************************** 
-    Returns node that matches condition
+    Returns node where value is inserted
+        i.e. node with data less than value param
+        i.e. node with data greater than value param
 ****************************************************/
-treeNode* TraverseTreeForValue(int value, bool greaterThanOrEqual, treeNode * node)
+treeNode* InsertValueIntoCorrectPositionInTree(int value, treeNode * node)
 {
-    if (greaterThanOrEqual)
+    if (node->Parent == NULL && node == NULL)
     {
-        if (value < node->Parent->data)
-            return TraverseTreeForValue(value, greaterThanOrEqual, node->Parent);
-
+        node = new treeNode(value, false);
+        return node;
     }
+
+    if (node == NULL)
+    {
+        std::cout << "No null non-new-root nodes allowed" << '\n';
+        return NULL;
+    }
+
+    bool greaterThanNode = value > node->data;
+    bool equalToNode = value == node->data;
+
+    if (equalToNode && node->Parent)
+    {
+        treeNode* newNode = new treeNode(value, node->onRightSideOfTree);
+        if (node->Left == NULL)
+        {
+            node->Left = newNode;
+            return node->Left;
+        }
+
+        if (node->Right == NULL)
+        {
+            node->Right = newNode;
+            return node->Right;
+        }
+
+        newNode->Left = node->Left;
+        InsertValueIntoCorrectPositionInTree(node->Left->data, node->Parent);
+            // TODO --> may need to do recursive call here to find position for node that has been moved, starting from nearby position, either the
+                // parent or the node that was just added
+        delete node->Left;
+        node->Left = newNode;
+    }
+
+    if (equalToNode && node->Parent == NULL)
+    {
+        treeNode* newNode = new treeNode(value, node->onRightSideOfTree);
+        if (node->Left == NULL)
+        {
+            node->Left = newNode;
+            node->Left->onRightSideOfTree = false;
+            return node->Left;
+        }
+
+        if (node->Right == NULL)
+        {
+            node->Right = newNode;
+            node->Right->onRightSideOfTree = true;
+            return node->Right;
+        }
+    }
+
+    if (node->onRightSideOfTree)
+    {
+        if (greaterThanNode && !equalToNode)
+        {
+            if (node->Right == NULL)
+            {
+                node->Right = new treeNode(value, node, node->onRightSideOfTree);
+                return node->Right;
+            }
+            
+            return InsertValueIntoCorrectPositionInTree(value, node->Right);
+        }
+
+        if (greaterThanNode && equalToNode)
+        {
+            if (node->Right == NULL)
+            {
+
+            }
+        }
+    }
+
+
+    if (node->data >= value)
+    {
+        if (node->onRightSideOfTree)
+        {
+
+        }
+    }
+
     
+
      
 }
 
