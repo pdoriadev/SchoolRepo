912757792b9825ebcf1b46505a15cc0af4c1b972
Peter Doria <pdoriadev@gmail.com>
2023-08-02 17:14:27 -0700

    Wrote example way to find a sequence of characters and overwrite the sequence without deleting other portions of the file

--- CS22-StrAndAlg/SelfStudy/Streams/Streams/Streams.cpp
+++ CS22-StrAndAlg/SelfStudy/Streams/Streams/Streams.cpp
@@ -26,20 +26,65 @@ int main()
     std::string messageFromFile = "";
     while (!inputStreamRhino.eof())
     {
-        std::getline(inputStreamRhino, messageFromFile); 
+        std::getline(inputStreamRhino, messageFromFile);
         // getline overwrites the string each call so we have to copy its information 
             // down now befor it gets overwritten on the next getline call
         std::cout << messageFromFile;
         /* new line character is not included when stream is read into string with getline()
             adding it in manually. Since the getline() terminates at the \n, we can add
             a new line character to the end of our std::cout. Alternatively, we can check for
-                if (!inputStreamRhino.eof() && !inputStreamRhino.failbit) 
+                if (!inputStreamRhino.eof() && !inputStreamRhino.failbit)
         */
         std::cout << "\n";
     }
     std::cout << std::flush;
     inputStreamRhino.close();
 
+    /* Find and replace specific sequence of characters in stream */
+    std::ifstream in("RhinoFile.txt");
+    std::string s = "";
+    std::streampos pinkPos;
+    while (in >> s)
+    {
+        // Find sequence of characters
+        if (s != "pink")
+        {
+            s = "";
+            continue;
+        }
 
+        // Once sequence is found, get position at beginning of sequence
+        for (char c : s)
+        {
+            in.unget();
+        }
+        pinkPos = in.tellg();
+        break;
+    }
+    in.close();
+
+    // Overwrite sequence of characters
+    std::ofstream out("RhinoFile.txt", std::ios::in | std::ios::out); 
+        /* https://cplusplus.com/reference/ios/ios_base/openmode/ofstream 
+            ofstream's default open mode is ios::trunc (or truncate). This deletes everything in the file before writing to it. 
+            ios::app (append) will only ever output to the end of file. std::out will delete anything after the output pos. 
+            To just overwrite a specific part of the file, we use open mode "std::ios::in | std::ios::out" 
+            Found this solution at: https://cplusplus.com/forum/beginner/166298/ */
+    out.seekp(pinkPos);
+    out << "blue";
+    out.close();
+
+    // Output edited file contents to console
+    in.open("RhinoFile.txt");
+    s = "";
+    std::cout << "\nEditedFile\n";
+    while (!in.eof())
+    {
+        std::getline(in, s);
+        std::cout << s << '\n';
+    }
+    std::cout << std::flush;
+    in.close();
+    
 }
 
