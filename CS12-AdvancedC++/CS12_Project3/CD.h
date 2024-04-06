320c49160580d1c0364db7c74f1dba6ec099eba9
Peter <pdoriadev@gmail.com>
2024-04-06 15:23:54 -0700

    Added getMonthlyStatement() functionality to CD

--- CS12-AdvancedC++/CS12_Project3/CD.h
+++ CS12-AdvancedC++/CS12_Project3/CD.h
@@ -109,6 +109,18 @@ public:
 
         bankAccount::withdraw(amount);
     }
+
+    std::string getMonthlyStatement()
+    {
+        return "Account Info: "
+                + std::string("\n    Account Type: Certificate of Deposit Account")
+                + std::string("\n    ") + std::string("Name: ") + gName()
+                + std::string("\n    ") + std::string("Account Number: ") + std::to_string(gAccountNumber())
+                + std::string("\n    ") + std::string("Balance: $") + std::to_string(gBalance())
+                + std::string("\n    ") + std::string("CD Period: ") + std::to_string(CDPeriod)
+                + std::string("\n    ") + std::string("Months til maturity: ") + std::to_string(gMonthsTilMatured())
+                + std::string("\n    ") + std::string("Interest: ") + std::to_string(interest) + "%";
+    }
 };
 
 #endif // CD_H
