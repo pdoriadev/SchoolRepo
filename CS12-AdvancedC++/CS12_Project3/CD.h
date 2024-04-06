3bf7881d80263b46fcf1362da3775c8bae4c7597
Peter <pdoriadev@gmail.com>
2024-04-06 15:12:45 -0700

    Unsigned int wraps around. Changed all unsigned ints to ints. Also restructured to use gMonthsTilMatured function. All that's left should be the getMonthlyStatement() function?

--- CS12-AdvancedC++/CS12_Project3/CD.h
+++ CS12-AdvancedC++/CS12_Project3/CD.h
@@ -5,10 +5,6 @@
 #include <ctime>
 #include <vector>
 
-
-
-
-
 ///////////////////////////////
 /*
 certificateOfDeposit
@@ -22,14 +18,15 @@ certificateOfDeposit
 class certificateOfDeposit : public bankAccount
 {
 protected:
-    double interest = gMIN_INTEREST();
-    unsigned int monthCreated = 0;
-    unsigned int yearCreated = 0;
-    unsigned int CDPeriod = 0;
-    inline static const std::string monthStrings[] = { "Jan", "Feb", "Mar", "Apr", "May"
-        "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+    inline static const std::string monthStrings[] = { "Jan", "Feb", "Mar",
+        "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
 
-    unsigned int gCurrentMonth()
+    double interest = gMIN_INTEREST();
+    int monthCreated = -1;
+    int yearCreated = -1;
+    int CDPeriod = -1;
+
+    int gCurrentMonth()
     {
         time_t now = time(0);
         std::string time = (std::ctime(&now));
@@ -45,48 +42,53 @@ protected:
         assert(false && "No matching month was found");
     }
 
-    unsigned int gCurrentYear()
+    int gCurrentYear()
     {
         time_t now = time(0);
         std::string time = (std::ctime(&now));
-        unsigned int year = std::stoul(time.substr(time.length() - 5, 4));
+        int year = std::stoul(time.substr(time.length() - 5, 4));
         assert (year >= yearCreated && "Current year cannot be year of CD creation.");\
         return year;
     }
+
 public:
 
     certificateOfDeposit(std::string _name, double _amount, double _interest,
-                         unsigned int _monthCreated, unsigned int _yearCreated, unsigned int _CDPeriod)
+                         int _monthCreated, int _yearCreated, int _CDPeriod)
     {
         bankAccount::init(_name);
+
         bankAccount::deposit(_amount);
+
         assert(_interest >= gMIN_INTEREST() && _interest <= gMAX_INTEREST() && "Interest is not valid value.");
         interest = _interest;
 
         assert(monthCreated > 0 && monthCreated <= 12 && "Month created is invalid");
         monthCreated = _monthCreated;
-        assert(_CDPeriod > 0 && _CDPeriod <= 12 && "Period of CD is invalid");
-        CDPeriod = _CDPeriod;
 
         assert(_yearCreated <= gCurrentYear() && "Year of creation cannot be after current year.");
         yearCreated = _yearCreated;
 
+        assert(_CDPeriod > 0 && _CDPeriod <= 12 && "Period of CD is invalid");
+        CDPeriod = _CDPeriod;
     }
 
 
     double gMIN_INTEREST() {return 1.01;}
     double gMAX_INTEREST() {return 1.99999;}
-    bool gIsMatured()
+
+
+    int gMonthsTilMatured()
     {
-        unsigned int currentYear = gCurrentYear();
-        unsigned int currentMonth = gCurrentMonth();
-        unsigned int monthsSinceCreation = 0;
+        int currentYear = gCurrentYear();
+        int currentMonth = gCurrentMonth();
+        int monthsSinceCreation = 0;
 
         if (yearCreated > currentYear)
         {
-            unsigned int monthsYearCreated = 13 - monthCreated;
-            unsigned int monthsInbetweenYears = (currentYear - yearCreated - 1) * 12;
-            unsigned int monthsThisYear = 13 - currentMonth;
+            int monthsYearCreated = 13 - monthCreated;
+            int monthsInbetweenYears = (currentYear - yearCreated - 1) * 12;
+            int monthsThisYear = 13 - currentMonth;
 
             monthsSinceCreation = monthsYearCreated + monthsInbetweenYears + monthsThisYear;
         }
@@ -95,17 +97,12 @@ public:
             monthsSinceCreation = 13 - currentMonth;
         }
 
-        if (monthsSinceCreation >= CDPeriod)
-        {
-            return true;
-        }
-
-        return false;
+        return CDPeriod - monthsSinceCreation;
     }
 
     result withdraw(double amount)
     {
-        if (!gIsMatured())
+        if (gMonthsTilMatured() > 0)
         {
             return result (false, "CD has not matured yet.");
         }
