686930e6490ea4a1bab3ef0bcd0e2262fe39e7ab
Peter <pdoriadev@gmail.com>
2024-04-08 19:50:52 -0700

    main.cpp: serviceChargeChecking test

--- CS12-AdvancedC++/CS12_Project3/main.cpp
+++ CS12-AdvancedC++/CS12_Project3/main.cpp
@@ -28,6 +28,10 @@ int main(int argc, char *argv[])
     accountsCSV.close();
 
     std::cout << "========================================\n"
+              << "================ Tests =================\n"
+              << "========================================\n\n";
+
+    std::cout << "========================================\n"
               << "========== Savings Accounts ============\n";
     std::vector<savingsAccount> * savingsAccounts = man.getSavingsAccounts();
     for (int i = 0; i < savingsAccounts->size(); i++)
@@ -66,6 +70,52 @@ int main(int argc, char *argv[])
     }
 
 
+    std::vector<digitalCheck*> checks;
+
+    std::cout << "========================================\n"
+              << "======= Service Charge Checking ========\n";
+    std::vector<serviceChargeChecking> * serviceChargeCheckingAccounts = man.getServiceChargeCheckingAccounts();
+    for (int i = 0; i < serviceChargeCheckingAccounts->size(); i++)
+    {
+        serviceChargeChecking * checking = &(serviceChargeCheckingAccounts->at(i));
+
+        std::cout << std::endl << checking->getMonthlyStatement()
+                  << std::endl;
+        // test service charge fucntionality
+        std::cout << '\n' << checking->payServiceCharge().gMessage();
+
+        // test writing check functionality
+        if (checking->gName() == "Alfred")
+        {
+            digitalCheck * c = checking->writeCheck("Amir", checking->gBalance());
+            if (c != NULL)
+            {
+                checks.push_back(c);
+            }
+        }
+        else if (checking->gName() == "Amir")
+        {
+            digitalCheck * c = checking->writeCheck("Helios", 500);
+            if (c != NULL)
+            {
+                checks.push_back(c);
+            }
+        }
+
+        // test receive check functionality
+        for (int i = 0; i < checks.size(); i++)
+        {
+            result r = checking->receiveCheck(*(checks[i]));
+            std::cout << '\n' << r.gMessage() << std::endl;
+            if (r.gSuccess())
+            {
+                checks.erase(checks.begin() + i);
+            }
+        }
+        std::cout << std::endl << serviceChargeCheckingAccounts->at(i).getMonthlyStatement()
+                  << std::endl;
+    }
+
 
     // Test different functionality of accounts
         // General
@@ -75,6 +125,7 @@ int main(int argc, char *argv[])
         // service charge Checking
             // write checks
             // receive (and validate) checks
+            // output check info
         // no service charge checking
             // write checks
             // receive and validate checks
