29eab71d8a12370e9cf2fed583f0902120eef225
Peter Doria <pdoriadev@gmail.com>
2024-05-29 04:02:29 -0700

    CS31_FinalProject - Added comment for simulatedTypePrinting function

--- Spring2024/CS31-Python/CS31_FinalProject/utils.py
+++ Spring2024/CS31-Python/CS31_FinalProject/utils.py
@@ -1,4 +1,8 @@
 import time
+
+###########################################################################
+# uses the sleep function in-betwen each char output to brute-force a retro
+#   real-time screen typing/loading.
 def simulatedTypePrinting(output :str, timeScale :float = 1):
     if (len(output) == 0):
         return 
